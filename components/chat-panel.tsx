2cac251aa8ed9957d632c20c6e7911be2eb1d020
JoeyLi2023 <151755198+JoeyLi2023@users.noreply.github.com>
2024-03-27 19:05:49 +0800

    Update chat-panel.tsx

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -36,24 +36,24 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
+      heading: 'What is your',
+      subheading: 'target occupation and industry?',
+      message: `What is your target occupation and industry?`
     },
     {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
+      heading: 'Help me',
+      subheading: 'improve my resume',
+      message: 'Help me improve my resume'
     },
     {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
+      heading: 'How to describe',
+      subheading: 'my management experience',
+      message: `How to describe my management experience`
     },
     {
-      heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      heading: 'What aspects of your resume',
+      subheading: `would you like to optimiz?`,
+      message: `What aspects of your resume would you like to optimiz?`
     }
   ]
 
